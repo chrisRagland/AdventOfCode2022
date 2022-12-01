ada0674f7d5862642d4731113b15603f1e75efe1
Chris <Chris@LAPTOP-CV16A31D>
2022-11-30 20:36:29 -0900

    Add project files.

---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace AdventOfCode2022
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Day1();
+		}
+
+		public static void Day1()
+		{
+			var day1Input = File.ReadAllLines(@"Day1.txt");
+			var values = new List<int>();
+
+			int currentValue = 0;
+
+			foreach (var item in day1Input)
+			{
+				if (string.IsNullOrEmpty(item))
+				{
+					values.Add(currentValue);
+					currentValue = 0;
+				}
+				else
+				{
+					currentValue += int.Parse(item);
+				}
+			}
+
+			Console.WriteLine($"Day 1 Part 1 Solution: {values.Max()}");
+			Console.WriteLine($"Day 1 Part 2 Solution: {values.OrderByDescending(x => x).Take(3).Sum()}");
+		}
+	}
+}
